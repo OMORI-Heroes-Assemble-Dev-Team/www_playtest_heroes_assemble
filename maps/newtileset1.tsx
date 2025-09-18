1ef71a5b375fedb658bb7c5497479c3e5e059853
Nomu716 <collaborations85@gmail.com>
2025-09-18 10:00:08 +0800

    Removed

---
+++ maps/newtileset1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="SciFiModernInTC" tilewidth="32" tileheight="32" tilecount="576" columns="24">
+ <image source="../img/tilesets/SciFiModernInTC.png" width="768" height="768"/>
+</tileset>
