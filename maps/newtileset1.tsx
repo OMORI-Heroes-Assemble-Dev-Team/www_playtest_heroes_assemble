1b749725898427b9c658637121ef3501179154d9
Nomu716 <collaborations85@gmail.com>
2025-09-18 17:18:23 +0800

    New map

--- maps/newtileset1.tsx
+++ maps/newtileset1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="SciFiModernInTC" tilewidth="32" tileheight="32" tilecount="576" columns="24">
- <image source="../img/tilesets/SciFiModernInTC.png" width="768" height="768"/>
+<tileset name="SciFiModernInTC" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../img/tilesets/SciFiModernInTC.png" width="512" height="512"/>
 </tileset>
---
+++ maps/untitled2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="FA_InteriorTileset" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="../img/tilesets/FA_InteriorTileset.png" width="1024" height="1024"/>
+</tileset>
