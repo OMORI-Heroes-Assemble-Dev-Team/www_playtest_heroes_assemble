2088eaaf2e204a8587a95c058520cdb3eb1f7744
Nomu716 <collaborations85@gmail.com>
2025-09-18 17:53:42 +0800

    More changes

---
+++ maps/ship.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="ship" tilewidth="32" tileheight="32" tilecount="483" columns="21">
+ <image source="../img/tilesets/ship.png" width="699" height="742"/>
+</tileset>
