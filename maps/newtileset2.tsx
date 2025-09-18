ab683ee04d75066b0609d27bba1f9e58bac42767
Nomu716 <collaborations85@gmail.com>
2025-09-18 21:47:25 +0800

    SO MUCH CHANGES

--- maps/newtileset2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="SciFiModernInTA5" tilewidth="32" tileheight="32" tilecount="128" columns="8">
- <image source="../img/tilesets/SciFiModernInTA5.png" width="256" height="512"/>
-</tileset>
